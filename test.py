a6781ace76fe7e62172ce86e55632fd91e836594
Mihailo Matejic <mihailo.matejic@gmail.com>
2018-08-17 16:54:23 +0200

    Tredovi, brzina, vreme

--- test.py
+++ test.py
@@ -5,50 +5,64 @@ import pygame
 
 pygame.init()
 
-prozor = pygame.display.set_mode((500, 500))
+prozor = pygame.display.set_mode((1000, 500))
 pozadina = pygame.image.load("background.png")
 pygame.display.set_caption("Igrica he he")
 isJump = False
-jumpCount = 8
-##pozadina je 980px
+jumpCount = 9
+delay = 0.002
+pozadinaCounter = 0
 class Igrac():
     x = 50
     y = 420
     velicina = 20
     boja = (255, 0, 0)
     brzina = 1
-    hitbox = pygame.Rect(x, y, 20, 20)
+    hitbox = pygame.Rect(x, y, velicina, velicina)
 
 class Prepreka():
-    x = 520
-    y = 420
-    velicina = 20
+    x = 1020
+    y = 390
+    velicina = 50
     boja = (0, 0, 255)
-    brzina = 1
-    hitbox = pygame.Rect(x, y, 20, 20)
+    brzina = 5
+    hitbox = pygame.Rect(x, y, velicina, velicina)
+    slika = pygame.image.load('kutija.jpg')
 
     def __init__(self):
-        Thread(target=pomerajPrepreku, args=(self, ), daemon=True).start()
+        Thread(target=pomerajPrepreku, args=(self, )).start()
+
+def uvecavajBrzinu():
+    global delay
+    while run:
+        time.sleep(1)
+        delay *= 0.99
 
 def pomerajPrepreku(p):
+    global delay
+    print(delay)
+    global pozadinaCounter
     while run:
-        time.sleep(0.003)
-        if p.x < -20:
-            p.x = 520
+        time.sleep(delay)
+        print(delay)
+        if p.x < -50:
+            p.x = 550
         p.x -= p.brzina
+        pozadinaCounter -= 1
+        if pozadinaCounter < -981:
+            pozadinaCounter = 0
 
 def skoci():
     global isJump
     global jumpCount
-
     global t1
-    brojac = 18
+    brojac = 20
     while brojac >= 0:
         time.sleep(0.03)
         if not isJump:
             isJump = True
         else:
-            if jumpCount >= -8:
+            if jumpCount >= -9:
                 neg = 1
                 if jumpCount < 0:
                     neg = -1
@@ -56,32 +70,86 @@ def skoci():
                 jumpCount -= 1
             else:
                 isJump = False
-                jumpCount = 8
+                jumpCount = 9
         brojac -= 1
     t1 = Thread(target=skoci, args=())
+
+def osveziEkran():
+    global delay
+    global pozadinaCounter
+    prozor.fill((255, 255, 255))
+    prepreka.hitbox = pygame.Rect(prepreka.x + prepreka.velicina, prepreka.y, prepreka.velicina, prepreka.velicina)
+    igrac.hitbox = pygame.Rect(igrac.x + prepreka.velicina, igrac.y, 20, 20)
+    prozor.blit(pozadina, (pozadinaCounter, 0))
+    text_to_screen(prozor, ('Brzina: ' + str(delay)))
+    pygame.draw.rect(prozor, igrac.boja, (igrac.x, igrac.y, igrac.velicina, igrac.velicina))
+    prozor.blit(prepreka.slika, (prepreka.x, prepreka.y))
+    pygame.display.flip()
+
+def text_to_screen(screen, text, x = 50, y = 50, size = 50, color = (200, 000, 000)):
+    text = str(text)
+    font = pygame.font.SysFont('Comic Sans MS', size)
+    text = font.render(text, True, color)
+    screen.blit(text, (x, y))
+
+def gameOver():
+    global delay
+    global run
+    #global delay
+    gameOver = pygame.image.load('gameOver.png')
+    prozor.fill((0, 0, 0))
+    playAgainRect = pygame.Rect(150, 300, 75, 75)
+    quitRect = pygame.Rect(275, 300, 75, 75)
+    prozor.blit(gameOver, (100, 166))
+    prozor.blit(pygame.image.load('playAgain.png'), (150, 300))
+    prozor.blit(pygame.image.load('quit.png'), (275, 300))
+    pygame.display.flip()
+    cekaj = True
+    while cekaj:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Set the x, y postions of the mouse click
+                x, y = event.pos
+                if playAgainRect.collidepoint(x, y):
+                    cekaj = False
+                    delay = 0.002
+                    main()
+                elif quitRect.collidepoint(x, y):
+                    run = False
+                    cekaj = False
+                    pygame.quit()
+                    quit()
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+
+
 run = True
 igrac = Igrac()
 prepreka = Prepreka()
 t1 = Thread(target=skoci, args=())
-pozadinaCounter = 0
-while run:
-    pygame.time.delay(1)
+brzinaTred = Thread(target=uvecavajBrzinu, args=()).start()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] and not t1.isAlive():
-        t1.start()
-    if igrac.hitbox.colliderect(prepreka.hitbox):
-        run = False
-    prozor.fill((255, 255, 255))
-    prepreka.hitbox = pygame.Rect(prepreka.x+prepreka.velicina, prepreka.y, 20, 20)
-    igrac.hitbox = pygame.Rect(igrac.x+prepreka.velicina, igrac.y, 20, 20)
-    prozor.blit(pozadina, (pozadinaCounter, 0))
-    pozadinaCounter -= 1
-    if pozadinaCounter < -981:
-        pozadinaCounter = 0
-    pygame.draw.rect(prozor, igrac.boja, (igrac.x, igrac.y, igrac.velicina, igrac.velicina))
-    pygame.draw.rect(prozor, prepreka.boja, (prepreka.x, prepreka.y, prepreka.velicina, prepreka.velicina))
-    pygame.display.flip()
+def main():
+    global run
+    global delay
+    prepreka.x = 520
+    prepreka.y = 390
+
+    osveziEkran()
+    while run:
+        pygame.time.delay(1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not t1.isAlive():
+            t1.start()
+        if igrac.hitbox.colliderect(prepreka.hitbox):
+            gameOver()
+        osveziEkran()
+
+main()
