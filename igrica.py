8f1bfefbd20f87f04316c52254a5e758f3894e29
Mihailo Matejic <mihailo.matejic@gmail.com>
2018-08-17 18:04:40 +0200

    Reimenovao

--- igrica.py
+++ igrica.py
@@ -5,13 +5,15 @@ import pygame
 
 pygame.init()
 
+kasnjenjeKonst = 0.003
 prozor = pygame.display.set_mode((1000, 500))
 pozadina = pygame.image.load("background.png")
-pygame.display.set_caption("Igrica he he")
+pygame.display.set_caption("Jumping car")
 isJump = False
 jumpCount = 9
-delay = 0.002
+delay = kasnjenjeKonst
 pozadinaCounter = 0
+poeni = 0
 class Igrac():
     x = 50
     y = 420
@@ -37,6 +39,11 @@ def uvecavajBrzinu():
     while run:
         time.sleep(1)
         delay *= 0.95
+def tredPoeni():
+    global poeni
+    while run:
+        time.sleep(delay)
+        poeni += 1
 
 def pomerajPrepreku(p):
     global delay
@@ -75,16 +82,17 @@ def skoci():
 def osveziEkran():
     global delay
     global pozadinaCounter
+    global poeni
     prozor.fill((255, 255, 255))
     prepreka.hitbox = pygame.Rect(prepreka.x + prepreka.velicina, prepreka.y, prepreka.velicina, prepreka.velicina)
     igrac.hitbox = pygame.Rect(igrac.x + prepreka.velicina, igrac.y, 20, 20)
     prozor.blit(pozadina, (pozadinaCounter, 0))
-    text_to_screen(prozor, ('Brzina: ' + str("%.5f" % delay)))
+    poeniPrint(prozor, ('Poeni: ' + str(poeni)))
     pygame.draw.rect(prozor, igrac.boja, (igrac.x, igrac.y, igrac.velicina, igrac.velicina))
     prozor.blit(prepreka.slika, (prepreka.x, prepreka.y))
     pygame.display.flip()
 
-def text_to_screen(screen, text, x = 50, y = 50, size = 50, color = (200, 000, 000)):
+def poeniPrint(screen, text, x = 5, y = 5, size = 15, color = (0, 0, 0)):
     text = str(text)
     font = pygame.font.SysFont('Comic Sans MS', size)
     text = font.render(text, True, color)
@@ -93,6 +101,7 @@ def text_to_screen(screen, text, x = 50, y = 50, size = 50, color = (200, 000, 0
 def gameOver():
     global delay
     global run
+    global poeni
     gameOver = pygame.image.load('gameOver.png')
     prozor.fill((0, 0, 0))
     playAgainRect = pygame.Rect(150, 300, 75, 75)
@@ -111,7 +120,8 @@ def gameOver():
                 x, y = event.pos
                 if playAgainRect.collidepoint(x, y):
                     cekaj = False
-                    delay = 0.003
+                    delay = kasnjenjeKonst
+                    poeni = 0
                     main()
                 elif quitRect.collidepoint(x, y):
                     run = False
@@ -129,12 +139,11 @@ igrac = Igrac()
 prepreka = Prepreka()
 t1 = Thread(target=skoci, args=())
 brzinaTred = Thread(target=uvecavajBrzinu, args=()).start()
+poeniTred = Thread(target=tredPoeni, args=()).start()
 
 def main():
     global run
-    global delay
-    prepreka.x = 520
-    prepreka.y = 390
+    global delay   
 
     osveziEkran()
     while run:
